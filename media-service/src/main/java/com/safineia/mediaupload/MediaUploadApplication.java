fa199454873c53ef9175fc68a8942165d7f6e163
Salim Rahmani <salimrahmani10@gmail.com>
2022-11-02 18:52:54 +0100

    add discovery service

--- media-service/src/main/java/com/safineia/mediaupload/MediaUploadApplication.java
+++ media-service/src/main/java/com/safineia/mediaupload/MediaUploadApplication.java
@@ -2,11 +2,11 @@ package com.safineia.mediaupload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MediaUploadApplication {
-
-
       public static void main(String[] args) {
             SpringApplication.run(MediaUploadApplication.class,args);
       }
